1e0511a342a062673f1fecbd9518d53b6b8e0f64
Dimitri <dimitri.asseray@gmail.com>
2016-01-13 16:30:35 +0100

    init workspace

---
+++ a-algorithm/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <QtCore>
+#include "petri.h"
+
+using namespace std;
+
+void loadCsv(QList<QStringList > * sequences, QString fileName){
+   sequences->clear();
+   QFile file(fileName);
+   if (file.open(QIODevice::ReadOnly | QIODevice::Text)){
+       while (!file.atEnd()){
+           QString line = file.readLine();
+            sequences->append(line.trimmed().split(QChar(','), QString::SkipEmptyParts));
+       }
+       file.close();
+   }
+}
+
+int main()
+{
+    QList<QStringList> sequences;
+    loadCsv(&sequences, "../data/data.csv");
+    Petri P(sequences);
+    // Make Tl, Ti and To
+    P.listTransitions();
+    //Enumerate Xl places
+    P.enumerateXl();
+    //Reduce Xl to get Yl
+    P.reduceXl();
+    //List arcs to get Fl
+    P.enumerateFl();
+
+    //Print
+    P.printSequences();
+    P.printTransitions();
+    P.printFootprints();
+    P.printPlaces();
+    P.printArcs();
+    return 0;
+}
+
+
